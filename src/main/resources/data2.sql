3f6d412c71499c45bdca75841adb68afe5293455
suryaranjan <ranjansurya86@gmail.com>
2026-06-27 14:54:13 +0530

    Your commit message

--- src/main/resources/data2.sql
+++ src/main/resources/data2.sql
@@ -1,18 +1,18 @@
 -- Sample data for classeses
 INSERT INTO classes (name) VALUES ('LKG');
 INSERT INTO classes (name) VALUES ('UKG');
-INSERT INTO classes (name) VALUES ('1st');
-INSERT INTO classes (name) VALUES ('2nd');
-INSERT INTO classes (name) VALUES ('3rd');
-INSERT INTO classes (name) VALUES ('4th');
-INSERT INTO classes (name) VALUES ('5th');
-INSERT INTO classes (name) VALUES ('6th');
-INSERT INTO classes (name) VALUES ('7th');
-INSERT INTO classes (name) VALUES ('8th');
-INSERT INTO classes (name) VALUES ('9th');
-INSERT INTO classes (name) VALUES ('10th');
-INSERT INTO classes (name) VALUES ('11th');
-INSERT INTO classes (name) VALUES ('12th');
+INSERT INTO classes (name) VALUES ('1ST');
+INSERT INTO classes (name) VALUES ('2ND');
+INSERT INTO classes (name) VALUES ('3RD');
+INSERT INTO classes (name) VALUES ('4TH');
+INSERT INTO classes (name) VALUES ('5TH');
+INSERT INTO classes (name) VALUES ('6TH');
+INSERT INTO classes (name) VALUES ('7TH');
+INSERT INTO classes (name) VALUES ('8TH');
+INSERT INTO classes (name) VALUES ('9TH');
+INSERT INTO classes (name) VALUES ('10TH');
+INSERT INTO classes (name) VALUES ('11TH');
+INSERT INTO classes (name) VALUES ('12TH');
 
 -- Sample data for schools
 INSERT INTO school (name, address, state, district, pin, phone, payment_mode, session_start_month,session_end_month, logo_url)
